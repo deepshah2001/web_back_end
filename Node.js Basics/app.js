3b9e145b1d0957129b85100cb790d62251730716
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-08-08 19:54:21 +0530

    Node.js Request-Response

--- Node.js Basics/app.js
+++ Node.js Basics/app.js
@@ -2,7 +2,44 @@ const http = require('http');
 const portNumber = 4000;
 
 const server = http.createServer((req, res) => {
-    console.log("Deep Shah");
+    const url = req.url;
+
+    if(url === '/') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Server</title></head>');
+        res.write('<body><h1>Hello from my first Node.js Server!</h1></body>');
+        res.write('</html>');
+        return res.end();
+    } else if(url === '/home') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Server</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    } else if(url === '/about') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Server</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    } else if(url === '/node') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Server</title></head>');
+        res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    } else {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Server</title></head>');
+        res.write('<body><h1>Enter Valid URL</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 server.listen(portNumber, 'localhost', () => 
