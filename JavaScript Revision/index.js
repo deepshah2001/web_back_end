5a465e5652078bcd2904aa8677647200dc5a3cd1
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-08-08 17:12:14 +0530

    Javascript Basics Revision

--- JavaScript Revision/index.js
+++ JavaScript Revision/index.js
@@ -3,19 +3,43 @@
 const a = 2;
 const b = 7;
 
-const product = (a, b) => (`Product of ${a} and ${b} is ${a*b}`);
+const product = (a, b) => `Product of ${a} and ${b} is ${a * b}`;
 
 console.log(product(a, b));
 
 // Creating object for student
 
 const student = {
-    name: "Deep",
-    age: 21,
+  name: "Deep",
+  age: 21,
 
-    greet() {
-        console.log("Hi, I am ", this.name);
-    }
+  greet() {
+    console.log("Hi, I am ", this.name);
+  },
 };
 
 student.greet();
+
+// Arrays
+const array = ["apple", "oranges", " ", "mango", " ", "lemon"];
+// map
+const newArray = array.map((a) => (a === " ") ? "empty string" : a);
+
+console.log(array);
+console.log(newArray);
+
+// Spread operator
+const arr1 = ['hello', 'world']
+const arr2 = [...arr1];
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr1 == arr2);
+console.log(arr1 === arr2);
+
+// Rest operator
+const arr3 = (...args) => {
+    return args;
+}
+
+console.log(arr3(1, 2, 3, 4, 5, 6, 7));
