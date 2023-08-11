8a1e21989cc43085b248263c94d1620100915e85
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-08-11 19:47:51 +0530

    SQL using Sequelize

--- Express JS Course Project/models/product.js
+++ Express JS Course Project/models/product.js
@@ -1,30 +1,29 @@
-//  Database Connection
-const db = require('../util/database');
+// Using Sequelize
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const sequelize = require('../util/database');
 
-  save() {
-    return db.execute('INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)',
-    [this.title, this.imageUrl, this.price, this.description]);
+// Creating Model
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+})
 
-  static deleteProductById(id) {
-    return db.execute('DELETE FROM products WHERE products.id = ?', [id]);
-  }
-
-  static fetchAll() {
-    // Database executing query
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product;
--- Express JS Course Project/util/database.js
+++ Express JS Course Project/util/database.js
@@ -1,10 +1,21 @@
-const mysql = require('mysql2');
+// Using Sequelize
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'root'
+const sequelize = new Sequelize("node-complete", "root", "root", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// For connecting to database - mysql on our own by creating a pool of requests
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'root'
+// });
+
+// module.exports = pool.promise();
